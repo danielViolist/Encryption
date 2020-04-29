5f359bc916842963542029be80e7f31fad15db7f
cSwiggitySwooty <acolinstuart@gmail.com>
2020-04-28 23:43:13 -0400

    Made Atbash in cpp

--- Atbash/Atbash.cpp
+++ Atbash/Atbash.cpp
@@ -1,31 +1,72 @@
+#include <string>
 #include <iostream>
 
-std::string encode(std::string aMessage)
+std::string encode(std::string aMessage, int messageSize)
 {
-
+    std::string encodedMsg = "";
+    for(int i = 0; i < messageSize; i++)
+    {
+        if(aMessage[i] >= 65 && aMessage[i] < 97)
+        {
+            int temp = aMessage[i] - 'A';
+            encodedMsg += ('Z' - temp);
+        }
+        else if(aMessage[i] >= 97 && aMessage[i] < 123)
+        {
+            int temp = aMessage[i] - 'a';
+            encodedMsg += ('z' - temp);
+        }
+        else
+        {
+            encodedMsg += aMessage[i];
+        }
+    }
+    return encodedMsg;
 }
 
-std::string decode(std::string aMessage)
+std::string decode(std::string aMessage, int messageSize)
 {
-
+    std::string decodedMsg = "";
+    for(int i = 0; i < messageSize; i++)
+    {
+        if(aMessage[i] >= 65 && aMessage[i] < 97)
+        {
+            int temp = 'Z' - aMessage[i];
+            decodedMsg += ('A' + temp);
+        }
+        else if(aMessage[i] >= 97 && aMessage[i] < 123)
+        {
+            int temp = 'z' - aMessage[i];
+            decodedMsg += ('a' + temp);
+        }
+        else
+        {
+            decodedMsg += aMessage[i];
+        }
+    }
+    return decodedMsg;
 }
 
 int main()
 {
-    std::cout << "Do you want to encode, decode, or quit? Enter \'E\' to encode, \'D\' to decode, or \'Q\' to quit." << std::endl;
-    char c;
-    std::cin >> c;
     while(true)
-        {
+    {
+        std::cout << "Do you want to encode, decode, or quit? Enter \'E\' to encode, \'D\' to decode, or \'Q\' to quit." << std::endl;
+        char c;
+        std::cin >> c;
         if(c == 'E' || c == 'e')
         {
-            
+            std::cout << "Enter the message you wish to encode\n";
+            std::string msg;
+            std::cin >> msg;
+            std::cout << encode(msg, msg.length()) << std::endl;
         }
         else if(c == 'D' || c == 'd')
         {
             std::cout << "Enter the message you wish to decode\n";
             std::string msg;
             std::cin >> msg;
+            std::cout << decode(msg, msg.length()) << std::endl;
         }
         else if(c == 'Q' || c == 'q')
         {
