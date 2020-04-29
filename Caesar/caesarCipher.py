da1ea54483dba184142a2db0a8f47c2bbe8f5b4e
danielViolist <daniel.violist3@gmail.com>
2020-04-28 22:54:46 -0400

    fixed

--- Caesar/caesarCipher.py
+++ Caesar/caesarCipher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Ceasar Cipher Encryption and Decription
+# Caesar Cipher Encryption and Decription      
 class message:
     def __init__(self, msg="abc", key=1):
         self.__message = msg
