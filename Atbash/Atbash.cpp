c30403c29ae7b2fef3adb2ca30c606cc576c684e
cSwiggitySwooty <acolinstuart@gmail.com>
2020-04-28 23:05:46 -0400

    Updated some stuffs

--- Atbash/Atbash.cpp
+++ Atbash/Atbash.cpp
@@ -12,5 +12,25 @@ std::string decode(std::string aMessage)
 
 int main()
 {
-    std::cout << "Please "
+    std::cout << "Do you want to encode, decode, or quit? Enter \'E\' to encode, \'D\' to decode, or \'Q\' to quit." << std::endl;
+    char c;
+    std::cin >> c;
+    while(true)
+        {
+        if(c == 'E' || c == 'e')
+        {
+            
+        }
+        else if(c == 'D' || c == 'd')
+        {
+            std::cout << "Enter the message you wish to decode\n";
+            std::string msg;
+            std::cin >> msg;
+        }
+        else if(c == 'Q' || c == 'q')
+        {
+            std::cout << "Deuces homie" << std::endl;
+            return 0;
+        }
+    }
 }
