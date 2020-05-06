da6ee7cfafd3b925348de5b40badf8a887bf73e7
cSwiggitySwooty <acolinstuart@gmail.com>
2020-05-06 12:23:18 -0400

    Small changes to Vigenere.cpp

--- Vigenere/Vigenere.cpp
+++ Vigenere/Vigenere.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
+#include <string>
 
 char alphabet[] = {' ', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-int toUpper(char phrase[])
+std::string toUpper(std::string phrase)
 {
-    int i;
-    for(i = 0; i < 1024; i++)
+    std::string uppercasePhrase;
+    for(int i = 0; i < phrase.length(); i++)
     {
-        if(phrase[i] == 0)
-        {
-            break;
-        }
-        phrase[i] = toupper(phrase[i]);
+        uppercasePhrase += toupper(phrase[i]);
     }
-    return i;
+    return uppercasePhrase;
 }
 
-void cipher(char msg[], int msgLength, char key[], int keyLength, char direction)
+void cipher(std::string msg, std::string key, char direction)
 {
     int index;
+    //TODO --- Actually do the cipher lol
     if(direction == 'e')
     {
-        for(int i = 0; i < msgLength; i++)
-        {
-
-        }
+        
     }
     else
     {
-        for(int i = 0; i < msgLength; i++)
-        {
-
-        }
+        
     }
     std::cout << std::endl;
 }
@@ -39,18 +31,14 @@ void cipher(char msg[], int msgLength, char key[], int keyLength, char direction
 int main()
 {
     char selection;
-    char msg[1024];
+    std::string msg;
     int msgLength;
     int keyLength;
-    char key[1024];
+    std::string key;
     while(true)
     {
         //Clearing the arrays
-        for(int i = 0; i < 1024; i++)
-        {
-            msg[i] = key[i] = 0;
-        }
-        msgLength = keyLength = 0;
+        msg = key = "";
         std::cout << "Would you like to encrypt, decrypt, or quit? Enter 'e', 'd', or 'q': " << std::endl;
         std::cin >> selection;
         std::cin.ignore();
@@ -62,12 +50,12 @@ int main()
         else if(selection == 'e' || selection == 'd')
         {
             std::cout << "Enter the message: ";
-            std::cin.getline(msg, 1024);
-            std::cout << "\nEnter the key: ";
-            std::cin.getline(key, 1024);
-            msgLength = toUpper(msg);
-            keyLength = toUpper(key);
-            cipher(msg, msgLength + 1, key, keyLength + 1, selection);
+            std::getline(std::cin, msg);
+            std::cout << "Enter the key: ";
+            std::getline(std::cin, key);
+            msg = toUpper(msg);
+            key = toUpper(key);
+            cipher(msg, key, selection);
         }
     } 
 }
