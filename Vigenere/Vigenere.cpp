30d3e5407dff7a8a45bc9de3b11bc2300e4588a2
danielViolist <daniel.violist3@gmail.com>
2020-05-09 12:20:52 -0400

    Added 'A'

--- Vigenere/Vigenere.cpp
+++ Vigenere/Vigenere.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-char alphabet[] = {' ', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char alphabet[] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 std::string toUpper(std::string phrase)
 {
