4aa2e864c83bf1de38ae214274f9b1ac9c837cfc
Dan <daniel.violist3@gmail.com>
2021-06-12 13:16:42 -0400

    Add files via upload

--- Atbash/Atbash.py
+++ Atbash/Atbash.py
@@ -11,33 +11,19 @@ def encode(msg):
         if letter == " ":
             cipher += " "
             continue
-        char = original.get(letter)
-        cipher += backwards.get(char)
+        try:
+            char = original.get(letter)
+            cipher += backwards.get(char)
+        except Exception:
+            cipher += letter
     return cipher
-def decode(msg):
-    original = {"Z": 1, "Y": 2, "X": 3, "W": 4, "V": 5, "U": 6, "T": 7, "S": 8, "R": 9, "Q": 10, "P": 11, "O": 12, "N": 13, "M": 14, "L": 15, "K": 16, "J": 17, "I": 18,
-    "H": 19, "G": 20, "F": 21, "E": 22, "D": 23, "C": 24, "B": 25, "A": 26}
-    backwards = {26: "Z", 25: "Y", 24: "X", 23: "W", 22: "V", 21: "U", 20: "T", 19: "S", 18: "R", 17: "Q", 16: "P", 15: "O", 14: "N", 13: "M", 12: "L", 11: "K", 10: "J",
-     9: "I", 8: "H", 7: "G", 6: "F", 5: "E", 4: "D", 3: "C", 2: "B", 1: "A"}
-    cipher = msg.upper()
-    msg = ""
-    for letter in cipher:
-        if letter == " ":
-            msg += " "
-            continue
-        char = original.get(letter)
-        msg += backwards.get(char)
-    return msg
 
 while(True):
     mode = input("Would you like to ecrypt, decrypt, or quit? Enter 'e', 'd', or 'q': ")
-    if mode == 'e':
+    if mode == 'e' or mode == 'd' or mode == 'E' or mode == 'D':
         msg = input("Enter the message to encrypt: ")
         print(encode(msg))
-    elif mode == 'd':
-        msg = input("Enter the message to decode: ")
-        print(decode(msg))
-    elif mode == 'q':
+    elif mode == 'q' or mode == 'Q':
         break
     else:
         continue
